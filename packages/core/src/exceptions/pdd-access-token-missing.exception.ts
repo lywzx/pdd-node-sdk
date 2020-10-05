27513e5795b85d1534af7e4d0ccfe3f1a25a45be
lywzx <liuyang@lyblog.net>
2020-10-05 16:58:25 +0800

    fix(access token missing method): fix catch error service not correct

--- packages/core/src/exceptions/pdd-access-token-missing.exception.ts
+++ packages/core/src/exceptions/pdd-access-token-missing.exception.ts
@@ -4,12 +4,30 @@ import { PddBaseException } from './pdd-base.exception';
  * 未传入access token时的报错信息
  */
 export class PddAccessTokenMissingException extends PddBaseException {
-  constructor(message: string) {
+  /**
+   * 错误消息
+   * @param message
+   * @param isFromCache
+   */
+  constructor(message: string, private isFromCache = false) {
     super(message);
     Object.setPrototypeOf(this, PddAccessTokenMissingException.prototype);
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
     this.name = PddAccessTokenMissingException.name;
+  }
+
+  /**
+   * 当前错误是否从缓存中产生
+   * @param fromCache {boolean}
+   */
+  public fromCache(): boolean;
+  public fromCache(fromCache: boolean): void;
+  public fromCache(fromCache?: boolean): boolean | void {
+    if (typeof fromCache === 'undefined') {
+      return this.isFromCache;
+    }
+    this.isFromCache = fromCache;
   }
 }
